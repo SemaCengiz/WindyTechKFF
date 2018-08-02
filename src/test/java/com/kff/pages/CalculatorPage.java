36016f48543093ff48ee929186664db6b883abe1
aiperi1991 <tynybekovaa@gmail.com>
2018-08-02 16:49:25 -0500

    2test cases are passing

--- src/test/java/com/kff/pages/CalculatorPage.java
+++ src/test/java/com/kff/pages/CalculatorPage.java
@@ -1,14 +1,33 @@
 package com.kff.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.kff.utilities.Driver;
 
 public class CalculatorPage {
-
+	WebDriver driver = Driver.getDriver();
 	public CalculatorPage() {
-		PageFactory.initElements(Driver.getDriver(), this);
-
+		PageFactory.initElements(driver, this);
 	}
+
+		
+
+	
+	@FindBy(xpath= "//*[@id=\"subsidy-calculator-new\"]/div[3]/div/dl/dt/a[1]/h5")
+	public WebElement aboutThisToolButton;
+	
+	
+	@FindBy(xpath="//*[@id=\"subsidy-calculator-new\"]/div[6]/div/dl/dt/a[1]/h5")
+	public WebElement notesButton;
+	
+	@FindBy(xpath="//*[@id=\"subsidy-calculator-new\"]/div[7]/div/dl/dt/a[1]/h5")
+	public WebElement frequentlyAskedQuestionsButton;
+	
+	@FindBy(xpath="//*[@id=\"hs-eu-confirmation-button\"]")
+	public WebElement cookie;
+	
 
 }
--- src/test/java/com/kff/test/CalculatorTests.java
+++ src/test/java/com/kff/test/CalculatorTests.java
@@ -1,10 +1,45 @@
 package com.kff.test;
 
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import com.kff.pages.CalculatorPage;
 import com.kff.pages.StatesFactsPage;
+import com.kff.utilities.ConfigReader;
+import com.kff.utilities.Driver;
 
 public class CalculatorTests {
 
 	StatesFactsPage fact = new StatesFactsPage();
 	CalculatorPage calc = new CalculatorPage();
-}
+
+
+@Test
+public void accordionMenuTest() throws InterruptedException {
+	Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
+	
+	
+	
+    calc.aboutThisToolButton.click();
+    String actual = calc.aboutThisToolButton.getText();
+    String expected ="ABOUT THIS TOOL";
+    assertEquals(actual, expected);
+    
+    calc.cookie.click();
+    
+    calc.frequentlyAskedQuestionsButton.click();
+    String actual1 =calc.frequentlyAskedQuestionsButton.getText();
+    String expected1="FREQUENTLY ASKED QUESTIONS";
+    
+    assertEquals(actual1, expected1);
+ 	calc.notesButton.click();
+ 	String actual2 = calc.notesButton.getText();
+    String expected2 ="NOTES";
+    assertEquals(actual2, expected2);
+ 	
+ 	
+}}
