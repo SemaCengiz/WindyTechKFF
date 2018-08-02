9092741623fc13ac1a1c234c77362f391e32d987
hasanistan <hasanistan1@gmail.com>
2018-08-02 14:18:12 -0500

    added page initilization

--- src/test/java/com/kff/test/CalculatorTests.java
+++ src/test/java/com/kff/test/CalculatorTests.java
@@ -1,5 +1,10 @@
 package com.kff.test;
 
+import com.kff.pages.CalculatorPage;
+import com.kff.pages.StatesFactsPage;
+
 public class CalculatorTests {
 
+	StatesFactsPage fact = new StatesFactsPage();
+	CalculatorPage calc = new CalculatorPage();
 }
--- src/test/java/com/kff/test/StateFactTests.java
+++ src/test/java/com/kff/test/StateFactTests.java
@@ -1,5 +1,23 @@
 package com.kff.test;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import com.kff.pages.StatesFactsPage;
+import com.kff.utilities.ConfigReader;
+import com.kff.utilities.Driver;
+
 public class StateFactTests {
 
+	StatesFactsPage fact = new StatesFactsPage();
+// Test Case 1205	
+	@Test
+	public void verifyAboutStateHealthFactsLink() {
+		Driver.getDriver().get(ConfigReader.getProperties("urlstate"));
+		fact.aboutStateHealthFactsLink.click();
+		String actualTitle = Driver.getDriver().getTitle();
+		String expectedTitle = "About State Health Facts | The Henry J. Kaiser Family Foundation";
+		assertEquals(actualTitle, expectedTitle, "About State Health Facts link does not work");
+	}
 }
