065b34f5c01e8e1f386caa67cd83cf1e96b42ce0
hasanistan <hasanistan1@gmail.com>
2018-08-02 14:26:51 -0500

    fixed page object initiliazation

--- src/test/java/com/kff/test/StateFactTests.java
+++ src/test/java/com/kff/test/StateFactTests.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
 
+import com.kff.pages.CalculatorPage;
 import com.kff.pages.StatesFactsPage;
 import com.kff.utilities.ConfigReader;
 import com.kff.utilities.Driver;
@@ -11,13 +12,10 @@ import com.kff.utilities.Driver;
 public class StateFactTests {
 
 	StatesFactsPage fact = new StatesFactsPage();
-// Test Case 1205	
-	@Test
-	public void verifyAboutStateHealthFactsLink() {
-		Driver.getDriver().get(ConfigReader.getProperties("urlstate"));
-		fact.aboutStateHealthFactsLink.click();
-		String actualTitle = Driver.getDriver().getTitle();
-		String expectedTitle = "About State Health Facts | The Henry J. Kaiser Family Foundation";
-		assertEquals(actualTitle, expectedTitle, "About State Health Facts link does not work");
-	}
+	CalculatorPage calc = new CalculatorPage();
+
+	
+	
+	
+	
 }
