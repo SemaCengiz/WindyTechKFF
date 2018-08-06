839fac0013c32abf6ebda1687ccb628dba1cc712
rukia <rukia@10.1.10.101>
2018-08-06 18:44:25 -0500

    All test cases finished

--- src/test/java/com/kff/test/CalculatorTests.java
+++ src/test/java/com/kff/test/CalculatorTests.java
@@ -1,17 +1,16 @@
-package com.kff.test;
-
+package TestCases;
 
 import static org.testng.Assert.assertTrue;
 
-import java.awt.AWTException;
-import java.awt.Robot;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -25,121 +24,88 @@ public class CalculatorTests {
 
 	StatesFactsPage fact = new StatesFactsPage();
 	CalculatorPage calc = new CalculatorPage();
-	
 
+
+
+@Test // Rukia 1215 
+	public void NumberofChildren() {
+	
+	Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
+	calc.cookieAcceptButton.click();
+	Select sel = new Select(calc.numberOfChildren);
+	List<WebElement> options = sel.getOptions();
+	System.out.println(options.size());  
+	assertTrue(options.size()==4); 
+	sel.selectByIndex(1); 
+	assertTrue(calc.numberOfChildrenAge.isDisplayed(), "Age dropdown menu is not displayed");
+	assertTrue(calc.numberOfChildrenTobacco.isDisplayed(), "Tobacco dropdown menu is not displayed");
+	sel.selectByIndex(2); 
+	assertTrue(calc.numberOfChildrenAge2.isDisplayed(), "Age2 dropdown menu is not displayed");
+	assertTrue(calc.numberOfChildrenTobacco2.isDisplayed(), "Tobacco2 dropdown menu is not displayed");
+	sel.selectByIndex(3); 
+	assertTrue(calc.numberOfChildrenAge3.isDisplayed(), "Age3 dropdown menu is not displayed");
+	assertTrue(calc.numberOfChildrenTobacco3.isDisplayed(), "Tobacco3 dropdown menu is not displayed");
+}
+
+
+@Test // Rukia 1206 
+
+public void SelectState() {
+	
+	Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
+	Select stateselect= new Select(calc.selectState);
+	List<WebElement> stateoptions= stateselect.getOptions();
+	System.out.println(stateoptions.size());
+	assertTrue(stateoptions.size()==52);
+	
+	stateselect.selectByVisibleText("Illinois");
+	assertTrue(calc.zipCode.isDisplayed(), "Zipcode is not visible");
+	calc.zipCode.sendKeys("60640");
+}
+
+
+@Test // Rukia-Abai 1207
+
+public void SelectStateNegative() throws InterruptedException {
+	Thread.sleep(2000);
+	Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
+	Select stateselect= new Select(calc.selectState);
+	List<WebElement> stateoptions= stateselect.getOptions();
+	System.out.println(stateoptions.size());
+	assertTrue(stateoptions.size()==52);
+	
+	stateselect.selectByVisibleText("Alabama");
+	assertTrue(calc.zipCode.isDisplayed(), "Zipcode is not visible");
+	Thread.sleep(2000);
+	calc.zipCode.sendKeys("60640");
+}
+
+
+@Test // Rukia 1225
+public void ClearSubmit() throws InterruptedException {
+	
+	Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
+
+	calc.annualIncome.sendKeys("12345");
+	calc.submitButton.click(); 
+	Thread.sleep(1000);
+	assertTrue(calc.submitResultBox.isDisplayed(), "Submit button is not displayed");
+
+	Thread.sleep(2000);
+	calc.annualIncome.clear();
 	
 	
-// Test Case 1214
-	@Ignore
-	@Test
-	public void enterIncomeFunctionality() {
-		Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
-		calc.cookieAcceptButton.click();
-		assertTrue(calc.incomeAs.isEnabled());
-		assertTrue(calc.option2018Dollars.isSelected(), "2018 Dollars option is not selected");
-		calc.incomeAs.click();
-		assertTrue(calc.option2018Dollars.isDisplayed());
-		assertTrue(calc.optionpercentOfPoverty.isDisplayed());	
+	
 	}
-	
-	
-	
-	
 
-
-
-	@Ignore
-@Test
-public void accordionMenuTest() throws InterruptedException {
-	Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
-	
-	
-	
-    calc.aboutThisToolButton.click();
-    String actual = calc.aboutThisToolButton.getText();
-    String expected ="ABOUT THIS TOOL";
-    assertEquals(actual, expected);
-    
-    calc.cookie.click();
-    
-    calc.frequentlyAskedQuestionsButton.click();
-    String actual1 =calc.frequentlyAskedQuestionsButton.getText();
-    String expected1="FREQUENTLY ASKED QUESTIONS";
-    
-    assertEquals(actual1, expected1);
- 	calc.notesButton.click();
- 	String actual2 = calc.notesButton.getText();
-    String expected2 ="NOTES";
-    assertEquals(actual2, expected2);
- 	
- 	
-}
-
-//Test Case 1203
-	@Ignore
-@Test 
-public void embedInstruction() {
-	Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
-	
-	calc.cookieAcceptButton.click();
-	calc.embedInstructions.click();
-	
-	Driver.getDriver().navigate().to(ConfigReader.getProperties("urlembed"));
-	String actualTitle = Driver.getDriver().getTitle();
-	System.out.println(actualTitle);
-	String expectedTitle= "Health Insurance Marketplace Calculator Embed Instructions | The Henry J. Kaiser Family Foundation";
-	
-	assertEquals(actualTitle,expectedTitle );
-	
-	
-}
-
-//Test Case 1212
-	@Ignore
-@Test
-public void householdIncomeNegative() {
-	Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
-	
-	calc.cookieAcceptButton.click();
-	
-	calc.houseHoldIncome.sendKeys("abcde");
-	
-	calc.submitButton.click();
-	
-	String actual = calc.warningAfterSubmit.getText();
-	String expected = "Please enter a valid income.";
-	
-	assertEquals(actual, expected);
-	
-	
-}
-
-//Test Case 1221
-	
-@Test
-public void yesOrNoDropDown() throws AWTException {
-	Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
-	
-	calc.cookieAcceptButton.click();
-	
-	Robot rob= new Robot();
-	rob.mouseMove(2000, 1);
-	
-	
-	
-	System.out.println(calc.no.isSelected());
-	
-	
-	calc.yesOrNoDropDown.click();
-	
-	System.out.println(calc.no.isDisplayed());
-	
-	System.out.println(calc.yes.isDisplayed());
-	
-	
-	
-	
-}
-
+	@Test
+	public void clearButtons() {
+		Driver.getDriver().get(ConfigReader.getProperties("urlcalculator"));
+		calc.annualIncome.sendKeys("12345");
+		calc.clearButton.click();
+		String actual  = calc.annualIncome.getText();
+		String expected = "";
+		assertEquals(actual, expected);
+	}
 
 }
